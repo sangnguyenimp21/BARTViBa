b4715b9e981c0939803db0a9a984be4a20111524
phamnguyen97 <quocnguyenkh@gmail.com>
2022-09-25 17:03:08 +0700

    update readme

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from utils.logger import setup_logging
 
 if __name__ == "__main__":
     setup_logging()
-    uvicorn.run("apis.api:app", host="0.0.0.0", port=8001, reload=False, log_level="debug", debug=False,
+    uvicorn.run("apis.api:app", host="0.0.0.0", port=8000, reload=False, log_level="debug", debug=False,
                 workers=1, factory=False, loop="asyncio", timeout_keep_alive=120)
 
 
