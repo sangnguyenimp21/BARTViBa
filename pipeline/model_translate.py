4d0106e308e26ffb1e067bef47863af676338396
nguyenpq <nguyenpq@tmtco.asia>
2022-10-18 12:22:31 +0700

    update GPU

--- pipeline/model_translate.py
+++ pipeline/model_translate.py
@@ -10,8 +10,12 @@ from model.custom_mbart_model import CustomMbartModel
 class ModelTranslator(BaseServiceSingleton):
     def __init__(self, checkpoint_path: str = "pretrained/best_aligned"):
         super(ModelTranslator, self).__init__()
+        if torch.cuda.is_available():
+            self.device = "cuda:0"
+        else:
+            self.device = "cpu"
         self.tokenizer = AutoTokenizer.from_pretrained(checkpoint_path)
-        self.model = CustomMbartModel.from_pretrained(checkpoint_path)
+        self.model = CustomMbartModel.from_pretrained(checkpoint_path).to(self.device)
         self.model = self.model.eval()
 
     @staticmethod
@@ -27,7 +31,7 @@ class ModelTranslator(BaseServiceSingleton):
 
     def _translate(self, text):
         input_ids = self.tokenizer(text).input_ids
-        input_ids = torch.tensor([input_ids])
+        input_ids = torch.tensor([input_ids]).to(self.device)
         outputs = self.model.generate(input_ids=input_ids, num_beams=5, max_length=256, num_return_sequences=1)
         output = self.tokenizer.decode(outputs[0], skip_special_tokens=True)
         return output
